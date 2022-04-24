08223d086300f073eb3fe43dd53ecd03cb5d8dfc
David <lou.mazzeo.developer21@gmail.com>
2022-04-25 08:16:26 +1000

    Finished project.

--- The Gravity Box.swiftpm/HowTo.swift
+++ The Gravity Box.swiftpm/HowTo.swift
@@ -35,6 +35,8 @@ class HowTo: UIViewController {
         Your goal is to create a large element by collecting as many protons and neutrons from the accelerator as you can within 20 seconds. Collecting a particle is as easy as tapping it, but watch out - these particles decay and disappear quick, so they might be hard to tap on time!
         
         Your accelerator will take your extracted particles to create up to 16 different elements - each requiring their own amount of protons and neutrons to form. The largest element you've made will be saved for you to see where you're up to in your element discovery task.
+        
+        Have fun!
         """
         
         howToButton.configuration = .filled()
